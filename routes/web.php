9b1d853843a9bdfa766c60724dd890ab241b1fab
Roger Vergara <roger.jvs.08@gmail.com>
2023-01-11 11:29:14 -0500

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -128,24 +128,24 @@ Route::get('/', function (Request $request) {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('/home');
 
-Route::get('/categorias', [CategoriaController::class, 'index1'])->name('categorias');
-Route::get('/categoria/altabaja/{id}/{var}', [CategoriaController::class, 'altabaja'])->name('altabaja');
+Route::get('/categorias', [CategoriaController::class, 'index1'])->name('/categorias');
+Route::get('/categoria/altabaja/{id}/{var}', [CategoriaController::class, 'altabaja'])->name('/altabaja');
 Route::resource('categoria', CategoriaController::class);
 
-Route::get('/responsables', [ResponsableController::class, 'index1'])->name('responsables');
-Route::get('/responsable/altabaja/{id}/{var}', [ResponsableController::class, 'altabaja'])->name('altabaja');
+Route::get('/responsables', [ResponsableController::class, 'index1'])->name('/responsables');
+Route::get('/responsable/altabaja/{id}/{var}', [ResponsableController::class, 'altabaja'])->name('/altabaja');
 Route::resource('responsable', ResponsableController::class);
 
-Route::get('/incidencias', [IncidenciaController::class, 'index1'])->name('incidencias');
-Route::get('/incidencia/altabaja/{id}/{var}', [IncidenciaController::class, 'altabaja'])->name('altabaja');
+Route::get('/incidencias', [IncidenciaController::class, 'index1'])->name('/incidencias');
+Route::get('/incidencia/altabaja/{id}/{var}', [IncidenciaController::class, 'altabaja'])->name('/altabaja');
 Route::resource('incidencia', IncidenciaController::class);
 
-Route::get('/solucions', [SolucionController::class, 'index1'])->name('solucions');
-Route::get('/solucion/altabaja/{id}/{var}', [SolucionController::class, 'altabaja'])->name('altabaja');
+Route::get('/solucions', [SolucionController::class, 'index1'])->name('/solucions');
+Route::get('/solucion/altabaja/{id}/{var}', [SolucionController::class, 'altabaja'])->name('/altabaja');
 Route::resource('solucion', SolucionController::class);
 
-Route::get('/verResponsables/{id}', [CategoriaController::class, 'verResponsables'])->name('verResponsables');
+Route::get('/verResponsables/{id}', [CategoriaController::class, 'verResponsables'])->name('/verResponsables');
 
-Route::put('/asignarresponsable/{categoria_id}', [CategoriaController::class, 'asignarResponsables'])->name('asignarResponsables');
+Route::put('/asignarresponsable/{categoria_id}', [CategoriaController::class, 'asignarResponsables'])->name('/asignarResponsables');
