ffafdb82dae0bf9b9b6c4247c93c447bdd6759d3
Roger Vergara <roger.jvs.08@gmail.com>
2023-01-11 12:52:29 -0500

    Update web.php v1

--- routes/web.php
+++ routes/web.php
@@ -72,58 +72,60 @@ use Illuminate\Support\Facades\Session;
 
 
 Route::get('/', function (Request $request) {
-    $code = $request->query('code');
-    $client_id_oauth = env('CLIENT_ID_OAUTH');
-    if (!isset($code)) {
-        return Redirect::to('http://web.regionancash.gob.pe/api/oauth/authorize?response_type=code&client_id=' . $client_id_oauth);
-        return $code;
-    } else {
-        try {
-            $client = new Client();
-            $headers = [
-                'Content-Type' => 'text/plain'
-            ];
-            $request = new Psr7Request('POST', 'http://web.regionancash.gob.pe/api/auth/token', $headers, $code);
-            $res = $client->sendAsync($request)->wait();
-            $token = json_decode($res->getBody());
-            $token = $token->token;
 
-            $data = str_replace('_', '/', str_replace('-', '+', explode('.', $token)[1]));
-            $users = json_decode(base64_decode($data));
-            // return $users;
-            $uidSession = $users->uid;
-            $fullNameSession = $users->fullName;
-            $directorySession = $users->directory;
-            $userSession = $users->user;
+    // $code = $request->query('code');
+    // $client_id_oauth = env('CLIENT_ID_OAUTH');
+    // if (!isset($code)) {
+    //     return Redirect::to('http://web.regionancash.gob.pe/api/oauth/authorize?response_type=code&client_id=' . $client_id_oauth);
+    //     return $code;
+    // } else {
+    //     try {
+    //         $client = new Client();
+    //         $headers = [
+    //             'Content-Type' => 'text/plain'
+    //         ];
+    //         $request = new Psr7Request('POST', 'http://web.regionancash.gob.pe/api/auth/token', $headers, $code);
+    //         $res = $client->sendAsync($request)->wait();
+    //         $token = json_decode($res->getBody());
+    //         $token = $token->token;
 
-            if ($user = User::where('name', $users->user)->first()) {
-                Auth::login($user);
-                Session(['uid' => $uidSession]);
-                Session(['fullName' => $fullNameSession]);
-                Session(['directory' => $directorySession]);
-                Session(['user' => $userSession]);
+    //         $data = str_replace('_', '/', str_replace('-', '+', explode('.', $token)[1]));
+    //         $users = json_decode(base64_decode($data));
+    //         // return $users;
+    //         $uidSession = $users->uid;
+    //         $fullNameSession = $users->fullName;
+    //         $directorySession = $users->directory;
+    //         $userSession = $users->user;
 
-                $modulo = 'inicioAdmin';
+    //         if ($user = User::where('name', $users->user)->first()) {
+    //             Auth::login($user);
+    //             Session(['uid' => $uidSession]);
+    //             Session(['fullName' => $fullNameSession]);
+    //             Session(['directory' => $directorySession]);
+    //             Session(['user' => $userSession]);
 
-                return view('inicio.home', compact('modulo'));
-            } else {
+    //             $modulo = 'inicioAdmin';
 
-                $newUser = new User();
+    //             return view('inicio.home', compact('modulo'));
+    //         } else {
 
-                $newUser->nombres = $fullNameSession;
-                $newUser->name = $userSession;
-                $newUser->email = $userSession;
-                $newUser->password = Hash::make($userSession);
-                $newUser->remember_token = Hash::make($userSession);
-                $newUser->tipouser_id = '3';
-                $newUser->save();
+    //             $newUser = new User();
 
-                return view('/acceso');
-            }
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
-    }
+    //             $newUser->nombres = $fullNameSession;
+    //             $newUser->name = $userSession;
+    //             $newUser->email = $userSession;
+    //             $newUser->password = Hash::make($userSession);
+    //             $newUser->remember_token = Hash::make($userSession);
+    //             $newUser->tipouser_id = '3';
+    //             $newUser->save();
+
+    //             return view('/acceso');
+    //         }
+    //     } catch (\Throwable $th) {
+    //         return $th->getMessage();
+    //     }
+    // }
+
 });
 
 Auth::routes();
@@ -138,11 +140,11 @@ Route::get('/responsables', [ResponsableController::class, 'index1'])->name('res
 Route::get('/responsable/altabaja/{id}/{var}', [ResponsableController::class, 'altabaja'])->name('altabaja');
 Route::resource('responsable', ResponsableController::class);
 
-Route::get('/incidencias', [IncidenciaController::class, 'index1'])->name('/incidencias');
-Route::get('/incidencia/altabaja/{id}/{var}', [IncidenciaController::class, 'altabaja']);
-Route::resource('/incidencia', IncidenciaController::class);
+Route::get('/incidencias', [IncidenciaController::class, 'index1'])->name('incidencias');
+Route::get('/incidencia/altabaja/{id}/{var}', [IncidenciaController::class, 'altabaja'])->name('altabaja');
+Route::resource('incidencia', IncidenciaController::class);
 
-Route::get('solucions', [SolucionController::class, 'index1'])->name('solucions');
+Route::get('/solucions', [SolucionController::class, 'index1'])->name('solucions');
 Route::get('/solucion/altabaja/{id}/{var}', [SolucionController::class, 'altabaja'])->name('altabaja');
 Route::resource('solucion', SolucionController::class);
 
