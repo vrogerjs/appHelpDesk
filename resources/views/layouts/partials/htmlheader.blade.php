6caf88ae44cb4e313e70af5b66a98d040cfbb438
Roger Vergara <roger.jvs.08@gmail.com>
2023-01-12 09:00:06 -0500

    Librerias js y css

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -6,7 +6,9 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/vue.js') }}"></script>
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('js/sweetalert2@11.js') }}"></script>
+    {{-- <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
     <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
 
 </head>
--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,3 +1,5 @@
-<script src="{{asset('js/app.js')}}"></script> 
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script> 
-<script src="{{asset('js/toastr.min.js')}}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/all.min.js') }}"></script>
+<script src="{{ asset('js/jquery-3.3.1.js') }}"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script> --}}
+<script src="{{ asset('js/toastr.min.js') }}"></script>
